5cac27521cddfbf52b9e4f0c2417b72dd7aba30a
arslantoor <muhammadarslantoor@gmail.com>
2021-06-03 15:29:40 +0500

    added user session email as return responce in checklist controler

--- src/Controller/ChecklistController.php
+++ src/Controller/ChecklistController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class ChecklistController extends AbstractController
 {
@@ -12,12 +13,13 @@ class ChecklistController extends AbstractController
     /**
      * @Route ("/checklist", name="checklist")
      */
-    public function checklist()
+    public function checklist(UserInterface $user)
     {
+        $email =$user->getEmail();
         $task_list = ['Introduction','Training Overview',
             'Symfony Setup', 'Symfony Create Page', 'Routing', 'Controller', 'Templates', 'Configuration',
             'Forms', 'Database and Doctrine ORM', 'Services Container', 'Security', 'Logging', 'Validation',
             'Bundles', 'Console', 'Translations', 'Easy Admin bundle'];
-        return $this->render('checklist.html.twig',['task_list'=>$task_list]);
+        return $this->render('checklist.html.twig',['task_list'=>$task_list,'user_email'=>$email]);
     }
 }
