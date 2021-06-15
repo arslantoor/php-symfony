ee44881cfb5da292429128760e22886107613061
arslantoor <muhammadarslantoor@gmail.com>
2021-06-15 12:38:27 +0500

    fix issue with security.yml/ChecklistFormType remove dublicate provider & use of packages

--- src/Form/CheckListFormType.php
+++ src/Form/CheckListFormType.php
@@ -4,16 +4,10 @@ namespace App\Form;
 
 use App\Entity\Checklist;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Validator\Constraints\NotBlank;
-
 class CheckListFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
