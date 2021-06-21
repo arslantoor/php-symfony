d008b190f648867cf38d3c5004c36afe1a3529ae
arslantoor <muhammadarslantoor@gmail.com>
2021-06-21 13:01:46 +0500

    update redirect route to sonata-admin

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,7 @@ use app\Entity\User;
 use App\Repository\ProductRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -12,13 +13,14 @@ use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
 class HomeController extends AbstractController
 {
+
+
     /**
      * @Route ("/dashboard", name="dashboard")
      * @Security("is_granted('ROLE_ADMIN') or is_granted('ROLE_USER')")
      */
     public function index(UserInterface $user)
     {
-
         $entityManager = $this->getDoctrine()->getManager();
         $user_list = $entityManager->getRepository(User::class)->findAll();
         $total=count($user_list);
@@ -32,9 +34,7 @@ class HomeController extends AbstractController
         }
         else
         {
-            return $this->render('index.html.twig',[
-                'user_list'=>$user_list, 'user_email'=>$email,'total_user'=>$total
-            ]);
+            return $this->redirect('/admin/dashboard');
         }
         return $this->render('error/error-403.html.twig');
     }
